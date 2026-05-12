811707e22210469d535523b6534f4d2d54b8870b
Ubuntu <ubuntu@fv4p0iskfc25lbcccf7u.auto.internal>
2026-05-12 21:03:45 +0000

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123']
+ALLOWED_HOSTS = ['taskiyap.hopto.org', '158.160.250.117', 'localhost', '127.0.0.1']
 
 # Application definition
 
